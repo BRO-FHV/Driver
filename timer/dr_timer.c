fc9410c56faf8435795bd6e24c75b33185bcfcd4
sla89 <skullsplitter@die-optimisten.net>
2014-03-16 15:04:15 +0100

    added timer defines (not finished)

---
+++ timer/dr_timer.c
@@ -0,0 +1,10 @@
+/*
+ * Driver: dr_timer.c
+ * Part of BRO Project, 2014 <<https://github.com/BRO-FHV>>
+ *
+ * Created on: 16.03.2014
+ * Description: 
+ * TODO
+ */
+
+
---
+++ timer/dr_timer.h
@@ -0,0 +1,15 @@
+/*
+ * Driver: dr_timer.h
+ * Part of BRO Project, 2014 <<https://github.com/BRO-FHV>>
+ *
+ * Created on: 16.03.2014
+ * Description: 
+ * TODO
+ */
+
+#ifndef DR_TIMER_H_
+#define DR_TIMER_H_
+
+
+
+#endif /* DR_TIMER_H_ */
