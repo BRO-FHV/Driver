4dee05a1336827136398f2af27bc8492a4b21559
sla89 <skullsplitter@die-optimisten.net>
2014-03-16 16:39:03 +0100

    started to implement basic methods

--- timer/dr_timer.c
+++ timer/dr_timer.c
@@ -7,4 +7,145 @@
  * TODO
  */
 
+#include <inttypes.h>
+#include <hw_timer.h>
+#include <basic.h>
+#include "dr_timer.h"
 
+#define NUMBER_OF_TIMERS	8
+#define RESET_VALUE			0x00
+
+
+uint16_t timers[NUMBER_OF_TIMERS];
+
+/**
+ * \brief enable a timer
+ *
+ * \param timer
+ *
+ * \return 0 on success, -1 on failure
+ */
+int TimerEnable(Timer timer) {
+	if(0 != timers[timer]) {
+		return -1; //already configured
+	}
+
+	uint16_t baseAdr = GetTimerBaseAdr(timer);
+
+	if(-1 == baseAdr) {
+		return -1; //failure
+	}
+
+	if(1 == timer) {
+		//TODO enable
+	} else {
+		//TODO enable
+	}
+}
+
+/**
+ * \brief disable a timer
+ *
+ * \param timer
+ *
+ * \return 0 on success, -1 on failure
+ */
+int TimerDisable(Timer timer) {
+	uint16_t baseAdr = GetTimerBaseAdr(timer);
+
+	if(-1 == baseAdr) {
+		return -1; //failure
+	}
+
+	if(1 == timer) {
+		//TODO disable
+	} else {
+		//TODO disable
+	}
+
+	timers[timer] = 0;
+
+	return 0;
+}
+
+
+/**
+ * \brief reset a timer (counting state)
+ *
+ * \param timer
+ *
+ * \return 0 on success, -1 on failure
+ */
+int TimerReset(Timer timer) {
+	uint16_t baseAdr = GetTimerBaseAdr(timer);
+
+	if(-1 == baseAdr) {
+		return -1; //failure
+	}
+
+	if(1 == timer) {
+		reg32w(baseAdr, TIMER1_TCRR, RESET_VALUE);
+	} else {
+		reg32w(baseAdr, TIMER_TCRR, RESET_VALUE);
+	}
+
+	return 0;
+}
+
+/**
+ * \brief configure the compare mode and the auto reaload mode. Timermust be disabled
+ *
+ * \param timer
+ * \param compareModeOn
+ * \param autoReload
+ *
+ * \return 0 on success, -1 on failure
+ */
+int TimerConfigure(Timer timer, uint8_t compareModeOn, uint8_t autoReload) {
+	if(0 < timers[timer]) {
+		return -1;
+	}
+
+	uint16_t baseAdr = GetTimerBaseAdr(timer);
+
+	if(-1 == baseAdr) {
+		return -1; //failure
+	}
+
+	if(1 == timer) {
+		//TODO configure
+	} else {
+		//TODO configure
+	}
+}
+
+
+/**
+ * \brief PRIVATE: returns the corresponding memory address of a timer
+ *
+ * \param timer
+ *
+ * \return the address on success, -1 on failure
+ */
+uint16_t GetTimerBaseAdr(Timer timer) {
+	switch(timer) {
+	case TIMER0:
+		return TIMER_0;
+	case TIMER1MS:
+		return TIMER_1MS;
+	case TIMER2:
+		return TIMER_2;
+	case TIMER3:
+		return TIMER_3;
+	case TIMER4:
+		return TIMER_4;
+	case TIMER5:
+		return TIMER_5;
+	case TIMER6:
+		return TIMER_6;
+	case TIMER7:
+		return TIMER_7;
+	default:
+		return -1;
+	}
+}
--- timer/dr_timer.h
+++ timer/dr_timer.h
@@ -10,6 +10,12 @@
 #ifndef DR_TIMER_H_
 #define DR_TIMER_H_
 
+enum Timer { TIMER0 = 0, TIMER1MS, TIMER2, TIMER3, TIMER4, TIMER5, TIMER6, TIMER7 };
+
+int TimerEnable(Timer timer);
+int TimerDisable(Timer timer);
+int TimerReset(Timer timer);
+int TimerConfigure(Timer timer, uint8_t compareModeOn, uint8_t oneShot);
 
 
 #endif /* DR_TIMER_H_ */
