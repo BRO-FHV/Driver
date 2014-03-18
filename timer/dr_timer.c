31cb1fa168b5b53041e7bee28e5c2646ee32587a
sla89 <skullsplitter@die-optimisten.net>
2014-03-18 23:18:16 +0100

    implemented timer (not tested - cannot compile that shit, because library cannot be found? WTF?)

--- timer/dr_timer.c
+++ timer/dr_timer.c
@@ -15,52 +15,53 @@
 #define NUMBER_OF_TIMERS	8
 #define RESET_VALUE			0x00
 
-
 uint16_t timers[NUMBER_OF_TIMERS];
 
-/**
- * \brief enable a timer
- *
- * \param timer
- *
- * \return 0 on success, -1 on failure
- */
+uint32_t GetTimerBaseAdr(Timer timer);
+void SetIrqWakeenMode(uint32_t baseAdr, IrqWakeen irqwakeen, uint32_t irqWakeenRegister);
+void SetIrqMode(uint32_t baseAdr, IrqMode irqMode, uint32_t irqRegister);
+void ResetTimer(uint32_t baseAdr, uint32_t tmar, uint32_t tldr,  uint32_t twer,  uint32_t tisr,  uint32_t ttgr, uint32_t tclr, uint32_t tcrr);
+void TimerBasicConfiguration(uint32_t baseAdr, uint32_t tmar, uint32_t tldr,  uint32_t tisr,  uint32_t ttgr, uint32_t matchValue, uint32_t loadValue);
+void TimerConfigureCE(uint32_t baseAdr, uint32_t tclr, uint8_t enable);
+void TimerConfigureAR(uint32_t baseAdr, uint32_t tclr, uint8_t enable);
+
 int TimerEnable(Timer timer) {
 	if(0 != timers[timer]) {
-		return -1; //already configured
+		return -1; //already enabled
 	}
 
-	uint16_t baseAdr = GetTimerBaseAdr(timer);
+	uint32_t baseAdr = GetTimerBaseAdr(timer);
 
-	if(-1 == baseAdr) {
+	if(UINT32_MAX == baseAdr) {
 		return -1; //failure
 	}
 
 	if(1 == timer) {
-		//TODO enable
+		reg32wor(baseAdr, TIMER1_TCLR, 0x01);
 	} else {
-		//TODO enable
+		reg32wor(baseAdr, TIMER_TCLR, 0x01);
 	}
+
+	timers[timer] = 1;
+
+	return 0;
 }
 
-/**
- * \brief disable a timer
- *
- * \param timer
- *
- * \return 0 on success, -1 on failure
- */
 int TimerDisable(Timer timer) {
-	uint16_t baseAdr = GetTimerBaseAdr(timer);
+	if(1 != timers[timer]) {
+		return -1; //not enabled
+	}
 
-	if(-1 == baseAdr) {
+	uint32_t baseAdr = GetTimerBaseAdr(timer);
+
+	if(UINT32_MAX == baseAdr) {
 		return -1; //failure
 	}
 
 	if(1 == timer) {
-		//TODO disable
+		reg32w(baseAdr, TIMER1_TCLR, TCLR_ST);
 	} else {
-		//TODO disable
+		reg32w(baseAdr, TIMER_TCLR, TCLR_ST);
 	}
 
 	timers[timer] = 0;
@@ -68,84 +69,175 @@ int TimerDisable(Timer timer) {
 	return 0;
 }
 
-
-/**
- * \brief reset a timer (counting state)
- *
- * \param timer
- *
- * \return 0 on success, -1 on failure
- */
 int TimerReset(Timer timer) {
-	uint16_t baseAdr = GetTimerBaseAdr(timer);
+	uint32_t baseAdr = GetTimerBaseAdr(timer);
 
-	if(-1 == baseAdr) {
+	if(UINT32_MAX == baseAdr) {
 		return -1; //failure
 	}
 
+	//reset registers
 	if(1 == timer) {
-		reg32w(baseAdr, TIMER1_TCRR, RESET_VALUE);
+		ResetTimer(baseAdr, TIMER1_TMAR, TIMER1_TLDR, TIMER1_TWER, TIMER1_TISR, TIMER1_TTGR, TIMER1_TCLR, TIMER1_TCRR);
 	} else {
-		reg32w(baseAdr, TIMER_TCRR, RESET_VALUE);
+		ResetTimer(baseAdr, TIMER_TMAR, TIMER_TLDR, TIMER_IRQWAKEEN, TIMER_IRQSTATUS, TIMER_TTGR, TIMER_TCLR, TIMER_TCRR);
 	}
 
 	return 0;
 }
 
-/**
- * \brief configure the compare mode and the auto reaload mode. Timermust be disabled
- *
- * \param timer
- * \param compareModeOn
- * \param autoReload
- *
- * \return 0 on success, -1 on failure
- */
-int TimerConfigure(Timer timer, uint8_t compareModeOn, uint8_t autoReload) {
+// TODO: change compareModeOn,  autoReloadOn,... to boolean..
+// TODO: prescaler, timer source
+int TimerConfigure(Timer timer, uint8_t enableCompareMode, uint8_t enableAutoReload, IrqMode irqMode, uint32_t matchValue, uint32_t loadValue, uint16_t clockSource, uint8_t pre, uint8_t ptv, IrqWakeen irqwakeen) {
 	if(0 < timers[timer]) {
 		return -1;
 	}
 
-	uint16_t baseAdr = GetTimerBaseAdr(timer);
+	uint32_t baseAdr = GetTimerBaseAdr(timer);
 
-	if(-1 == baseAdr) {
+	if(UINT32_MAX == baseAdr) {
 		return -1; //failure
 	}
 
+	//TMAR should be less than 0xFFFF FFFF
+	if(UINT32_MAX == matchValue) {
+		return -1;
+	}
+
+	TimerReset(timer);
+
 	if(1 == timer) {
-		//TODO configure
+		//timer 1 ms
+		TimerBasicConfiguration(baseAdr, TIMER1_TMAR, TIMER1_TLDR, TIMER1_TISR, TIMER1_TTGR, matchValue, loadValue);
+
+		TimerConfigureCE(baseAdr, TIMER1_TCLR, enableCompareMode);
+		TimerConfigureAR(baseAdr, TIMER1_TCLR, enableAutoReload);
+
+		SetIrqWakeenMode(baseAdr, irqwakeen, TIMER1_TWER);
+		SetIrqMode(baseAdr, irqMode, TIMER1_TIER);
+
+		//reset counter register
+		reg32w(baseAdr, TIMER1_TCRR, RESET_VALUE);
 	} else {
-		//TODO configure
+		//timer 0, 2 - 7
+		TimerBasicConfiguration(baseAdr, TIMER_TMAR, TIMER_TLDR, TIMER_IRQSTATUS, TIMER_TTGR, matchValue, loadValue);
+
+		TimerConfigureCE(baseAdr, TIMER_TCLR, enableCompareMode);
+		TimerConfigureAR(baseAdr, TIMER_TCLR, enableAutoReload);
+
+		SetIrqWakeenMode(baseAdr, irqwakeen, TIMER_IRQWAKEEN);
+		SetIrqMode(baseAdr, irqMode, TIMER_IRQENABLE_SET);
+	}
+
+	//reset counter register
+	reg32w(baseAdr, 1 == timer ? TIMER1_TCRR : TIMER_TCRR, RESET_VALUE);
+
+	return 0;
+}
+
+int TimerInterrupt(Timer timer) {
+	//TODO impl
+	return 0;
+}
+
+void TimerConfigureCE(uint32_t baseAdr, uint32_t tclr, uint8_t enable) {
+	if(1 == enable) {
+		reg32w(baseAdr, tclr, TCLR_CE);
+	} else {
+		reg32wxor(baseAdr, tclr, TCLR_CE);
 	}
 }
 
+void TimerConfigureAR(uint32_t baseAdr, uint32_t tclr, uint8_t enable) {
+	if(1 == enableAutoReload) {
+		reg32wor(baseAdr, TIMER_TCLR, TCLR_AR);
+	} else {
+		reg32wxor(baseAdr, TIMER_TCLR, TCLR_AR);
+}
 
-/**
- * \brief PRIVATE: returns the corresponding memory address of a timer
- *
- * \param timer
- *
- * \return the address on success, -1 on failure
- */
-uint16_t GetTimerBaseAdr(Timer timer) {
+void TimerBasicConfiguration(uint32_t baseAdr, uint32_t tmar, uint32_t tldr,  uint32_t tisr,  uint32_t ttgr, uint32_t matchValue, uint32_t loadValue) {
+	//defines the match value (e.g. interrupt is raised, if this value is reached)
+	reg32w(baseAdr, tmar, matchValue);
+	//defines where the timer should start to count (e.g. after a auto reload)
+	reg32w(baseAdr, tldr, loadValue);
+	//use default
+	reg32w(baseAdr, tisr, 0x03);
+	//Writing in the TTGR register, TCRR will be loaded from TLDR and prescaler counter will be cleared.
+	//Reload will be done regardless of the AR field value of TCLR register.
+	reg32w(baseAdr, ttgr, RESET_VALUE);
+}
+
+void SetIrqWakeenMode(uint32_t baseAdr, IrqWakeen irqwakeen, uint32_t irqWakeenRegister) {
+	//Wakeup-enabled events taking place when module is idle will generate an asynchronous wakeup
+	switch(irqwakeen) {
+	case IrqWakeen_MAT_WUP_ENA:
+		reg32w(baseAdr, irqWakeenRegister, IRQWAKEEN_MAT_WUP_ENA);
+		break;
+	case IrqWakeen_OVF_WUP_ENA:
+		reg32w(baseAdr, irqWakeenRegister, IRQWAKEEN_OVF_WUP_ENA);
+		break;
+	case IrqWakeen_TCAR_WUP_ENA:
+		reg32w(baseAdr, irqWakeenRegister, IRQWAKEEN_TCAR_WUP_ENA);
+		break;
+	case IrqWakeen_ALL:
+		reg32w(baseAdr, irqWakeenRegister, IRQWAKEEN_MAT_WUP_ENA & IRQWAKEEN_OVF_WUP_ENA & IRQWAKEEN_TCAR_WUP_ENA);
+		break;
+	case IrqWakeen_NONE:
+		//TODO
+		break;
+	}
+}
+
+void SetIrqMode(uint32_t baseAdr, IrqMode irqMode, uint32_t irqRegister) {
+	switch (irqMode) {
+	case IrqMode_MATCH:
+		reg32w(baseAdr, irqRegister, IRQENABLE_MAT_EN_FLAG);
+		break;
+	case IrqMode_CAPTURE:
+		reg32w(baseAdr, irqRegister, IRQENABLE_TCAR_EN_FLAG);
+		break;
+	case IrqMode_OVERFLOW:
+		reg32w(baseAdr, irqRegister, IRQENABLE_OVF_EN_FLAG);
+		break;
+	case IrqMode_ALL:
+		reg32w(baseAdr, irqRegister, IRQENABLE_OVF_EN_FLAG & IRQENABLE_MAT_EN_FLAG & IRQENABLE_TCAR_EN_FLAG);
+		break;
+	case IrqMode_NONE:
+		//TODO
+	default:
+		break;
+	}
+}
+
+uint32_t GetTimerBaseAdr(Timer timer) {
 	switch(timer) {
-	case TIMER0:
+	case Timer_TIMER0:
 		return TIMER_0;
-	case TIMER1MS:
+	case Timer_TIMER1MS:
 		return TIMER_1MS;
-	case TIMER2:
+	case Timer_TIMER2:
 		return TIMER_2;
-	case TIMER3:
+	case Timer_TIMER3:
 		return TIMER_3;
-	case TIMER4:
+	case Timer_TIMER4:
 		return TIMER_4;
-	case TIMER5:
+	case Timer_TIMER5:
 		return TIMER_5;
-	case TIMER6:
+	case Timer_TIMER6:
 		return TIMER_6;
-	case TIMER7:
+	case Timer_TIMER7:
 		return TIMER_7;
 	default:
-		return -1;
+		return UINT32_MAX;
 	}
+}
+
+void ResetTimer(uint32_t baseAdr, uint32_t tmar, uint32_t tldr,  uint32_t twer,  uint32_t tisr,  uint32_t ttgr, uint32_t tclr, uint32_t tcrr) {
+	reg32w(baseAdr, tmar, RESET_VALUE);
+	reg32w(baseAdr, tldr, RESET_VALUE);
+	reg32w(baseAdr, twer, RESET_VALUE);
+	reg32w(baseAdr, tisr, RESET_VALUE);
+	reg32w(baseAdr, ttgr, RESET_VALUE);
+	reg32w(baseAdr, tclr, RESET_VALUE);
+	reg32w(baseAdr, tcrr, RESET_VALUE);
 }
--- timer/dr_timer.h
+++ timer/dr_timer.h
@@ -10,12 +10,54 @@
 #ifndef DR_TIMER_H_
 #define DR_TIMER_H_
 
-enum Timer { TIMER0 = 0, TIMER1MS, TIMER2, TIMER3, TIMER4, TIMER5, TIMER6, TIMER7 };
-
+typedef enum { Timer_TIMER0 = 0, Timer_TIMER1MS, Timer_TIMER2, Timer_TIMER3, Timer_TIMER4, Timer_TIMER5, Timer_TIMER6, Timer_TIMER7 } Timer;
+typedef enum { IrqMode_MATCH = 0, IrqMode_OVERFLOW, IrqMode_CAPTURE, IrqMode_ALL, IrqMode_NONE } IrqMode;
+typedef enum { IrqWakeen_MAT_WUP_ENA = 0, IrqWakeen_OVF_WUP_ENA, IrqWakeen_TCAR_WUP_ENA, IrqWakeen_ALL, IrqWakeen_NONE } IrqWakeen;
+/**
+ * \brief enable a timer
+ *
+ * \param timer
+ *
+ * \return 0 on success, -1 on failure
+ */
 int TimerEnable(Timer timer);
+
+/**
+ * \brief disable a timer
+ *
+ * \param timer
+ *
+ * \return 0 on success, -1 on failure
+ */
 int TimerDisable(Timer timer);
+
+/**
+ * \brief reset a timer (counting state)
+ *
+ * \param timer
+ *
+ * \return 0 on success, -1 on failure
+ */
 int TimerReset(Timer timer);
-int TimerConfigure(Timer timer, uint8_t compareModeOn, uint8_t oneShot);
 
+/**
+ * \brief configure the compare mode and the auto reaload mode. Timermust be disabled
+ *
+ * \param timer
+ * \param compareModeOn
+ * \param autoReload
+ *
+ * \return 0 on success, -1 on failure
+ */
+int TimerConfigure(Timer timer, uint8_t enableCompareMode, uint8_t enableAutoReload, IrqMode irqMode, uint32_t matchValue, uint32_t loadValue, uint16_t clockSource, uint8_t pre, uint8_t ptv, IrqWakeen irqwakeen);
 
+/**
+ * \brief
+ *
+ * \param timer
+ * \param interrupt routine
+ *
+ * \return 0 on success, -1 on failure
+ */
+int TimerInterrupt(Timer timer);
 #endif /* DR_TIMER_H_ */
