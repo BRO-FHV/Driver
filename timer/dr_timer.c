a57689d3cf5e964da12a4d844d1a568cb86d568b
sla89 <skullsplitter@die-optimisten.net>
2014-03-20 21:42:31 +0100

    made code pretty and added documentation for public methods

--- timer/dr_timer.c
+++ timer/dr_timer.c
@@ -4,7 +4,16 @@
  *
  * Created on: 16.03.2014
  * Description: 
- * TODO
+ * This is the implementation file of the timer driver. It
+ * offers different methods for configuration and enabling/disabling
+ * a specific timer.
+ *
+ * A already enabled timer cannot be enabled or configured again. It must
+ * be disabled first.
+ *
+ * All timers are configured with auto-reload, capture enabled, posted mode and
+ * to raise an interrupt when the set time (passed as parameter to TimerConfiguration method=
+ * is over.
  */
 #include <stdio.h>
 #include <inttypes.h>
@@ -18,13 +27,12 @@
 #define RESET_VALUE			0x00
 
 /**
- * \brief   This macro will check for write POSTED status
+ * \brief   This macro will check for write POSTED status.
  *
- * \param   tsicr
- * \param   twps
+ * \param   tsicr		Timer Synchronous Interface Control Register of a specific timer
+ * \param   twps		Timer Write Posting Bits Register of a specific timer
  * \param   reg			Register whose status has to be checked
- * \param   baseAdd
-
+ * \param   baseAdd		Base address of the timer
  **/
 #define WaitForWrite(tsicr, twps, reg, baseAdd) if(reg32r(baseAdd, tsicr) & TSICR_POSTED)\
             										while((reg & (reg32r(baseAdd, twps))));
@@ -34,6 +42,7 @@ uint16_t timers[NUMBER_OF_TIMERS];
 typedef enum {
 	IrqMode_MATCH = 0, IrqMode_OVERFLOW, IrqMode_CAPTURE, IrqMode_ALL
 } IrqMode;
+
 typedef enum {
 	IrqWakeen_MAT_WUP_ENA = 0,
 	IrqWakeen_OVF_WUP_ENA,
@@ -53,15 +62,32 @@ void DisableCore(Timer timer, uint32_t baseAddr, uint32_t tclr, uint32_t tsicr, 
 void ResetCore(uint32_t baseAddr, uint32_t tmar, uint32_t tldr, uint32_t twer, uint32_t tisr, uint32_t ttgr, uint32_t tclr, uint32_t tcrr, uint32_t tsicr, uint32_t twps);
 void ConfigurationCore(uint32_t baseAddr, uint32_t tmar, uint32_t tldr,	uint32_t tisr, uint32_t ttgr, uint32_t tclr, uint32_t twer, uint32_t tier, uint32_t tsicr, uint32_t twps);
 
+void ClearTimer0IrqStatus();
+void ClearTimer1IrqStatus();
+void ClearTimer2IrqStatus();
+void ClearTimer3IrqStatus();
+void ClearTimer4IrqStatus();
+void ClearTimer5IrqStatus();
+void ClearTimer6IrqStatus();
+void ClearTimer7IrqStatus();
+void ClearTimerCore(uint32_t baseAddr, uint32_t tisr);
+
+/**
+ * \brief Enable a timer. ST bit of TCLR is set to 1. No registers were reset!
+ *
+ * \param timer Timer that should be enabled.
+ *
+ * \return 0 on success, -1 on failure
+ */
 int32_t TimerEnable(Timer timer) {
-	if (0 != timers[timer]) {
+	if (1 == timers[timer]) {
 		return -1; //already enabled
 	}
 
 	uint32_t baseAddr = GetTimerBaseAddr(timer);
 
 	if (UINT32_MAX == baseAddr) {
-		return -1; //failure
+		return -1; //timer does not exist
 	}
 
 	uint32_t tcrr = 1 == timer ? TIMER1_TCRR : TIMER_TCRR;
@@ -79,14 +105,27 @@ int32_t TimerEnable(Timer timer) {
 }
 
 void EnableCore(Timer timer, uint32_t baseAddr, uint32_t tclr, uint32_t tsicr, uint32_t twps) {
+	//turn on timer
 	reg32wor(baseAddr, tclr, TCLR_ST);
 
 	WaitForWrite(tsicr, twps, TWPS_W_PEND_TCLR, baseAddr)
 
+	//set to enabled
 	timers[timer] = 1;
 }
 
+/**
+ * \brief Disable a timer. ST bit of TCLR is set to 0. No registers were reset!
+ *
+ * \param timer Timer that should be disabled.
+ *
+ * \return 0 on success, -1 on failure
+ */
 int32_t TimerDisable(Timer timer) {
+	if (0 == timers[timer]) {
+		return -1; //already disabled
+	}
+
 	uint32_t baseAddr = GetTimerBaseAddr(timer);
 
 	if (UINT32_MAX == baseAddr) {
@@ -103,15 +142,27 @@ int32_t TimerDisable(Timer timer) {
 }
 
 void DisableCore(Timer timer, uint32_t baseAddr, uint32_t tclr, uint32_t tsicr, uint32_t twps) {
+	//shut down timer
 	reg32an(baseAddr, tclr, TCLR_ST);
 
 	WaitForWrite(tsicr, twps, TWPS_W_PEND_TCLR, baseAddr)
 
-	//TODO Disable interrupts
+	//disable interrupt routine
+	uint32_t irqCode = GetTimerInterruptCode(timer);
+	IntUnRegister(irqCode, routine);
+	IntHandlerDisable(irqCode);
 
+	//set to disabled
 	timers[timer] = 0;
 }
 
+/**
+ * \brief Reset a timer (e.g. counting state, interrupt status,..)
+ *
+ * \param timer Timer that should be reset.
+ *
+ * \return 0 on success, -1 on failure
+ */
 int32_t TimerReset(Timer timer) {
 	uint32_t baseAddr = GetTimerBaseAddr(timer);
 
@@ -137,7 +188,6 @@ void ResetCore(uint32_t baseAddr, uint32_t tmar, uint32_t tldr, uint32_t twer, u
 	WaitForWrite(tsicr, twps, TWPS_W_PEND_TLDR, baseAddr);
 
 	reg32wor(baseAddr, twer, RESET_VALUE);
-	reg32wor(baseAddr, tisr, RESET_VALUE);
 
 	reg32wor(baseAddr, ttgr, RESET_VALUE);
 	WaitForWrite(tsicr, twps, TWPS_W_PEND_TTGR, baseAddr);
@@ -147,12 +197,22 @@ void ResetCore(uint32_t baseAddr, uint32_t tmar, uint32_t tldr, uint32_t twer, u
 
 	reg32wor(baseAddr, tcrr, RESET_VALUE);
 	WaitForWrite(tsicr, twps, TWPS_W_PEND_TCRR, baseAddr);
+
+	ClearTimerCore(baseAddr, tisr);
 }
 
-//TODO feature wish: no prescaler - pass e.g. 500 ms, 1000 ms,...
+/**
+ * \brief Configure the passed timer to raise an interrupt (after passed milliseconds).
+ *
+ * \param timer	Timer that should be configured.
+ * \param milliseconds Time slice after which the interrupt is raised.
+ * \param routine This routine is called to handle the interrupt.
+ *
+ * \return 0 on success, -1 on failure
+ */
 int32_t TimerConfiguration(Timer timer, uint32_t milliseconds, InterruptRoutine routine) {
-	if (0 < timers[timer] || NULL == routine) {
-		return -1;
+	if (1 == timers[timer] || NULL == routine || 0 >= milliseconds) {
+		return -1; //timer is already enabled or routine is not set
 	}
 
 	uint32_t baseAddr = GetTimerBaseAddr(timer);
@@ -161,6 +221,7 @@ int32_t TimerConfiguration(Timer timer, uint32_t milliseconds, InterruptRoutine 
 		return -1; //failure
 	}
 
+	//TODO feature wish: no prescaler - pass e.g. 500 ms, 1000 ms,...
 //	//TMAR should be less than 0xFFFF FFFF
 //	if(UINT32_MAX == matchValue) {
 //		return -1;
@@ -203,8 +264,9 @@ void ConfigurationCore(uint32_t baseAddr, uint32_t tmar, uint32_t tldr,	uint32_t
 	reg32wor(baseAddr, tldr, 0x00);
 	WaitForWrite(tsicr, twps, TWPS_W_PEND_TLDR, baseAddr)
 
-	//use default
-	reg32wor(baseAddr, tisr, 0x03);
+	//clear pending interrupts
+	reg32an(baseAdr, tisr, TISR_ALL_FLAGS);
+
 	//Writing in the TTGR register, TCRR will be loaded from TLDR and prescaler counter will be cleared.
 	//Reload will be done regardless of the AR field value of TCLR register.
 	reg32wor(baseAddr, ttgr, RESET_VALUE);
@@ -218,21 +280,6 @@ void ConfigurationCore(uint32_t baseAddr, uint32_t tmar, uint32_t tldr,	uint32_t
 	SetIrqMode(baseAddr, IrqMode_MATCH, tier);
 }
 
-//TODO Method that is passed as interrupt method
-//in this method clear the M-Bit of the Interrupt and finally call function method passed via timer interrupt configuration
-int32_t TimerInterruptFlagClear(Timer timer) {
-	return -1;
-}
-
-/**
- * \brief   Configure the posted mode of a timer.
- *
- * \param   baseAdd       Base Address of the timer
- * \param   postMode      TSICR Register
- *
- * \return  None.
- *
- **/
 void EnablePostedMode(uint32_t baseAddr, uint32_t tsicr) {
 	/* Clear the POSTED field of TSICR */
 	reg32an(baseAddr, tsicr, TSICR_POSTED);
@@ -241,8 +288,7 @@ void EnablePostedMode(uint32_t baseAddr, uint32_t tsicr) {
 	reg32m(baseAddr, tsicr, TSICR_POSTED);
 }
 
-void SetIrqWakeenMode(uint32_t baseAddr, IrqWakeen irqwakeen,
-		uint32_t irqWakeenRegister) {
+void SetIrqWakeenMode(uint32_t baseAddr, IrqWakeen irqwakeen, uint32_t irqWakeenRegister) {
 	//Wakeup-enabled events taking place when module is idle will generate an asynchronous wakeup
 	switch (irqwakeen) {
 	case IrqWakeen_MAT_WUP_ENA:
@@ -255,8 +301,7 @@ void SetIrqWakeenMode(uint32_t baseAddr, IrqWakeen irqwakeen,
 		reg32wor(baseAddr, irqWakeenRegister, IRQWAKEEN_TCAR_WUP_ENA);
 		break;
 	case IrqWakeen_ALL:
-		reg32wor(baseAddr, irqWakeenRegister,
-				IRQWAKEEN_MAT_WUP_ENA & IRQWAKEEN_OVF_WUP_ENA & IRQWAKEEN_TCAR_WUP_ENA);
+		reg32wor(baseAddr, irqWakeenRegister, IRQWAKEEN_MAT_WUP_ENA & IRQWAKEEN_OVF_WUP_ENA & IRQWAKEEN_TCAR_WUP_ENA);
 		break;
 	}
 }
@@ -273,8 +318,7 @@ void SetIrqMode(uint32_t baseAddr, IrqMode irqMode, uint32_t irqRegister) {
 		reg32wor(baseAddr, irqRegister, IRQENABLE_OVF_EN_FLAG);
 		break;
 	case IrqMode_ALL:
-		reg32wor(baseAddr, irqRegister,
-				IRQENABLE_OVF_EN_FLAG & IRQENABLE_MAT_EN_FLAG & IRQENABLE_TCAR_EN_FLAG);
+		reg32wor(baseAddr, irqRegister, IRQENABLE_OVF_EN_FLAG & IRQENABLE_MAT_EN_FLAG & IRQENABLE_TCAR_EN_FLAG);
 		break;
 	default:
 		break;
@@ -325,4 +369,41 @@ uint32_t GetTimerInterruptCode(Timer timer) {
 	default:
 		return UINT32_MAX;
 	}
+}
+
+void ClearTimer0IrqStatus() {
+	ClearTimerCore(TIMER_0, TIMER_IRQSTATUS);
+}
+
+void ClearTimer1IrqStatus() {
+	ClearTimerCore(TIMER_1, TIMER1_TISR);
+}
+
+void ClearTimer2IrqStatus() {
+	ClearTimerCore(TIMER_2, TIMER_IRQSTATUS);
+}
+
+void ClearTimer3IrqStatus() {
+	ClearTimerCore(TIMER_3, TIMER_IRQSTATUS);
+}
+
+void ClearTimer4IrqStatus() {
+	ClearTimerCore(TIMER_4, TIMER_IRQSTATUS);
+}
+
+void ClearTimer5IrqStatus() {
+	ClearTimerCore(TIMER_5, TIMER_IRQSTATUS);
+}
+
+void ClearTimer6IrqStatus() {
+	ClearTimerCore(TIMER_6, TIMER_IRQSTATUS);
+}
+
+void ClearTimer7IrqStatus() {
+	ClearTimerCore(TIMER_7, TIMER_IRQSTATUS);
+}
+
+void ClearTimerCore(uint32_t baseAddr, uint32_t tisr) {
+	//clear all pending interrupt flags
+	reg32an(baseAdr, tisr, TISR_ALL_FLAGS);
 }
--- timer/dr_timer.h
+++ timer/dr_timer.h
@@ -10,54 +10,25 @@
 #ifndef DR_TIMER_H_
 #define DR_TIMER_H_
 
-typedef enum { Timer_TIMER0 = 0, Timer_TIMER1MS, Timer_TIMER2, Timer_TIMER3, Timer_TIMER4, Timer_TIMER5, Timer_TIMER6, Timer_TIMER7 } Timer;
+typedef enum {
+	Timer_TIMER0 = 0,
+	Timer_TIMER1MS,
+	Timer_TIMER2,
+	Timer_TIMER3,
+	Timer_TIMER4,
+	Timer_TIMER5,
+	Timer_TIMER6,
+	Timer_TIMER7
+} Timer;
+
 typedef void (*InterruptRoutine)(void);
 
-/**
- * \brief enable a timer
- *
- * \param timer
- *
- * \return 0 on success, -1 on failure
- */
 int32_t TimerEnable(Timer timer);
 
-/**
- * \brief disable a timer
- *
- * \param timer
- *
- * \return 0 on success, -1 on failure
- */
 int32_t TimerDisable(Timer timer);
 
-/**
- * \brief reset a timer (counting state)
- *
- * \param timer
- *
- * \return 0 on success, -1 on failure
- */
 int32_t TimerReset(Timer timer);
 
-/**
- * \brief configure the compare mode and the auto reaload mode. Timermust be disabled
- *
- * \param timer
- * \param compareModeOn
- * \param autoReload
- *
- * \return 0 on success, -1 on failure
- */
-int32_t TimerConfiguration(Timer timer, uint32_t milliseconds, InterruptRoutine routine) ;
-
-/**
- * \brief clears the interrupt flag of the passed timer
- *
- * \param timer
- *
- * \return 0 on success, -1 on failure
- */
-int32_t TimerInterruptFlagClear(Timer timer);
+int32_t TimerConfiguration(Timer timer, uint32_t milliseconds, InterruptRoutine routine);
 
 #endif /* DR_TIMER_H_ */
